42a985e10524903a61068bee1bf9cc033f09f93c
Andrew Ribas <andrew.j.ribas@gmail.com>
2020-07-06 20:05:49 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,13 @@
 def oxford_comma(array)
+  case array.size
 
+  when 1
+    return array.first
+  when 2
+    return "#{array.first} and #{array.last}"
+  end
+
+
+  last = array.pop
+  return"#{array.join(", ")}, and #{last}"
 end
