c267985cd892fe7dc5073a2019dd8cf70711c0a0
Jackson Reynolds <jwreynold@gmail.com>
2020-07-07 16:48:52 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,17 @@
 def oxford_comma(array)
-
-end
+  if array.length == 1
+    array.join
+  elsif array.length == 2
+    array.join(' and ')
+  else
+    oxford_array = []
+    array.each_with_index do |word, index|
+      if index == array.length - 1
+        oxford_array.push("and #{word}")
+      else
+        oxford_array.push("#{word}, ")
+      end #if
+    end #each
+    oxford_array.join
+  end #if
+end #oxford_comma
