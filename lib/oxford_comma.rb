766d78cdacfcae5b8edb9cd17eca11eb7786eda9
Doug Schallmoser <dougschallmoser@mgail.com>
2020-07-07 16:28:15 -0700

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,12 @@
-def oxford_comma(array)
+require 'pry'
 
+def oxford_comma(array)
+    if array.length == 2
+        array.join(" and ")
+    elsif array.length >= 3
+        array[-1].insert(0, "and ")
+        array.join(", ")
+    else
+        array.join 
+    end
 end
