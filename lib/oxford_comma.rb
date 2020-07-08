2f18bc2cbac9e2574c62d734ec2db0a1a98425f6
Chindalath Traymany <chindalatht@gmail.com>
2020-07-08 06:07:50 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,12 @@
 def oxford_comma(array)
-
+  case array.size
+    when 1
+      array.join 
+    when 2
+      array.join(" and ")
+    when 3
+      array[0..1].join(", ") << ", and #{array[-1]}"
+    else
+      array[0..-2].join(", ") << ", and #{array[-1]}"
+  end
 end
