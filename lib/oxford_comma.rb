40cacb40cacc270fd83a8880b593dcb87a122db5
Emrich-Michael Perrier <emrichmp@gmail.com>
2020-07-09 01:24:14 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,11 @@
 def oxford_comma(array)
-
+  if array.length >= 3
+    last = array[-1]
+    last = "and #{last}"
+    array.pop
+    array << last
+    array.join(", ")
+  else
+    array.join(" and ")
+  end
 end
