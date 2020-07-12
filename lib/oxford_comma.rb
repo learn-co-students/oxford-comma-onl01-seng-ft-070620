814d7ebb84bb002fe93a7dcdc0221a288cb0eeff
Cameron Cousins <cameroncousins18@gmail.com>
2020-07-12 23:10:44 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,19 @@
-def oxford_comma(array)
+require "pry"
 
+
+def oxford_comma(array)
+  if array.length == 1
+    return array[0]
+  elsif array.length == 2
+    return array.join(' and ')
+  elsif array.length == 3
+    array[-1].insert(0,'and ')
+    return array.join(', ')
+  else 
+    array[-1].insert(0,'and ')
+    return array.join(', ')
+  end
 end
+
+
+
