ff4b40068e2a3f8a1b7ac9e5022fedab9d1b374c
Doug Schallmoser <dougschallmoser@mgail.com>
2020-07-15 14:09:13 -0700

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,4 +1,11 @@
-require 'pry'
+# require 'pry'
+
+
+## REWORK
+
+# 1. Write a #oxford_comma method that:
+#    -Takes in an array of string elements and
+#    -Converts it into a string using the Oxford comma
 
 def oxford_comma(array)
     if array.length == 2
@@ -6,7 +13,7 @@ def oxford_comma(array)
     elsif array.length >= 3
         array[-1].insert(0, "and ")
         array.join(", ")
-    else
-        array.join 
+    else 
+        array.join()
     end
-end
+end 
